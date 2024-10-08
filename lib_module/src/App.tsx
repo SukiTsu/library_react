fbb7d6fce220086be8f36328c0eb1cc7e32a9714
yesfab <fabien@Fabiens-Laptop.local>
2024-10-08 18:51:02 +0200

    modifs page presentation

--- lib_module/src/App.tsx
+++ lib_module/src/App.tsx
@@ -1,17 +1,15 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path='/counter' element={<MonComposant/>} />
-        
-
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
