9ff037eaea12288e131cf25727f54e0ad02de464
yesfab <fabien@Fabiens-Laptop.local>
2024-10-08 19:19:06 +0200

    ajout fichier contenu de la page (texte qu'on pourra appeler sur les diff pages)

---
+++ lib_module/src/data/resume-data.tsx
@@ -0,0 +1,55 @@
+export const RESUME_DATA = {
+  sidebar: {
+    atoms: [
+      {
+        name: "Input",
+      },
+      {
+        name: "Checkbox",
+      },
+      {
+        name: "Select",
+      },
+      {
+        name: "Radio",
+      },
+      {
+        name: "Toggle",
+      },
+      {
+        name: "Button",
+      },
+      {
+        name: "Loader",
+      },
+    ],
+    molecules: [
+      {
+        name: "Alert",
+      },
+      {
+        name: "Breadcrumb",
+      },
+      {
+        name: "Card",
+      },
+    ],
+    organismes: [
+      {
+        name: "Accordéon",
+      },
+      {
+        name: "Carousel",
+      },
+      {
+        name: "Modal",
+      },
+      {
+        name: "Tabs",
+      },
+      {
+        name: "Tableau",
+      },
+    ],
+  },
+};
