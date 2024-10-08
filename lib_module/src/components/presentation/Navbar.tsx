d8a097f2b473bb7c3cfdbf9fd4d18a18ce61de05
yesfab <fabien@Fabiens-Laptop.local>
2024-10-08 17:58:58 +0200

    presentation

---
+++ lib_module/src/components/presentation/Navbar.tsx
@@ -0,0 +1,11 @@
+import "../../assets/css/navbar.css";
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <h2>
+        <strong>coding / UI</strong>
+      </h2>
+    </div>
+  );
+}
---
+++ lib_module/src/pages/home.tsx
@@ -0,0 +1,12 @@
+import "../assets/css/home.css";
+import Navbar from "../components/presentation/Navbar";
+import Sidebar from "../components/presentation/Sidebar";
+
+export default function Home() {
+  return (
+    <div className="homepage">
+      <Navbar />
+      <Sidebar />
+    </div>
+  );
+}
