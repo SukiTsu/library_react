49b1a053510f03b55eb64f91155003966eb7a878
yesfab <fabien@Fabiens-Laptop.local>
2024-10-09 11:02:15 +0200

    ajout input.tsx pour la page de input

--- lib_module/src/pages/components/atoms/input/input.tsx
+++ lib_module/src/pages/components/atoms/input/input.tsx
@@ -1,3 +1,19 @@
-export default function nput() {
-  return <div>input</div>;
+import "../../../../assets/css/pages/input.css";
+import { RESUME_DATA } from "../../../../data/resume-data";
+
+import Navbar from "../../../../components/presentation/Navbar";
+import Sidebar from "../../../../components/presentation/Sidebar";
+
+export default function Input() {
+  return (
+    <div>
+      <Navbar />
+      <div className="body-input">
+        <Sidebar />
+        <div>
+          <h1>{RESUME_DATA.content.atoms.component1.name}</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
