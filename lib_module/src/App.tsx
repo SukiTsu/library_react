b019260cc5485c07488cc84348f3380c7383ebf2
yesfab <fabien@Fabiens-Laptop.local>
2024-10-10 10:00:03 +0200

    ajout liens vers pages composants

--- lib_module/src/App.tsx
+++ lib_module/src/App.tsx
@@ -14,6 +14,16 @@ import Toggle from "./pages/components/atoms/toggle/toggle";
 import Button from "./pages/components/atoms/button/button";
 import Loader from "./pages/components/atoms/loader/loader";
 
+import Alert from "./pages/components/molecules/alert/alert";
+import Breadcrumb from "./pages/components/molecules/breadcrumb/breadcrumb";
+import Card from "./pages/components/molecules/card/card";
+
+import Accordeon from "./pages/components/organismes/accordeon/accordeon";
+import Carousel from "./pages/components/organismes/carousel/carousel";
+import Modal from "./pages/components/organismes/modal/modal";
+import Tabs from "./pages/components/organismes/tabs/tabs";
+import Tableau from "./pages/components/organismes/tableau/tableau";
+
 function App() {
   return (
     <BrowserRouter>
@@ -26,6 +36,16 @@ function App() {
         <Route path="/atoms/toggle" element={<Toggle />} />
         <Route path="/atoms/button" element={<Button />} />
         <Route path="/atoms/loader" element={<Loader />} />
+
+        <Route path="/molecules/alert" element={<Alert />} />
+        <Route path="/molecules/breadcrumb" element={<Breadcrumb />} />
+        <Route path="/molecules/card" element={<Card />} />
+
+        <Route path="/organismes/accordeon" element={<Accordeon />} />
+        <Route path="/organismes/carousel" element={<Carousel />} />
+        <Route path="/organismes/modal" element={<Modal />} />
+        <Route path="/organismes/tabs" element={<Tabs />} />
+        <Route path="/organismes/tableau" element={<Tableau />} />
       </Routes>
     </BrowserRouter>
   );
