5b45f1e054cac5525e9a2d1ab59523c2875b8191
yesfab <fabien@Fabiens-Laptop.local>
2024-10-10 16:21:42 +0200

    ajout commentaires sur code composant sidebar

--- lib_module/src/components/presentation/Sidebar.tsx
+++ lib_module/src/components/presentation/Sidebar.tsx
@@ -2,18 +2,24 @@ import { Link } from "react-router-dom";
 import "../../assets/css/sidebar.css";
 import { RESUME_DATA } from "../../data/data";
 
+//Définition du type Components
 type Components = Record<string, { name: string; path: string }>;
+//Définition du type Category
 type Category = { name: string; components: Components };
 
 export default function Sidebar() {
+  // Récupération des données pour la sidebar à partir de RESUME_DATA
+  // Les données sont organisées par sections et catégories
   const sidebarData: Record<string, Category[]> = RESUME_DATA.sidebar;
 
   return (
     <div className="sidebar">
       <h2>Components List</h2>
 
+      {/* Boucle sur chaque section des données de la sidebar */}
       {Object.keys(sidebarData).map((sectionKey) => {
         const section = sidebarData[sectionKey as keyof typeof sidebarData];
+        // Boucle sur chaque catégorie pour les afficher
         return section.map((category) => (
           <div key={category.name} className="components-category">
             <h3>{category.name}</h3>
